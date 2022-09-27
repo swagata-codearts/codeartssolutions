0dab1729696a2ffb48ca1699de5fcd8242b78daa
swagata-codearts <swagata.codearts@gmail.com>
2022-09-27 10:33:05 +0530

    Add accordion structure

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,19 @@ jQuery('#custom-owl').owlCarousel({
     }
 });
 
+jQuery('.js-accordion__item').on('click', function () {
+  // set active class
+  jQuery(this).parent().find('.state-active').removeClass('state-active');
+  
+  jQuery(this).removeClass('state-collapsed').addClass('state-active');
+  
+  //collapse other items
+  jQuery('.js-accordion__item').not(this).addClass('state-collapsed');
+  
+  
+});
+
+
 jQuery(document).ready(function() {
     jQuery('.-accordion').asAccordion({
       namespace: '-accordion'
