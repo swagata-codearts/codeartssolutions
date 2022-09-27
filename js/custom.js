47e64be67b3fab203834e9f3668890aae8085545
supriyofrontend <supriyo.codearts@gmail.com>
2022-09-27 10:59:00 +0530

    Owl carousel added

--- js/custom.js
+++ js/custom.js
@@ -15,6 +15,22 @@ jQuery('#custom-owl').owlCarousel({
         }
     }
 });
+jQuery('#project-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:3
+      }
+  }
+})
 
 jQuery(document).ready(function() {
     jQuery('.-accordion').asAccordion({
