dcf24f9b30bda998adf8be1b467d8a2269ca3960
swagata-codearts <swagata.codearts@gmail.com>
2022-11-21 10:40:01 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -78,54 +78,15 @@ jQuery('#brand-carousel').owlCarousel({
 
 
 jQuery('.js-accordion__item').on('click', function () {
-  // set active class
   jQuery(this).parent().find('.state-active').removeClass('state-active');
-  
-
-  
-  
   jQuery(this).removeClass('state-collapsed').addClass('state-active');
-  // jQuery(this).children().find('.plus-icon').removeClass('plus-icon').addClass('minus-icon');
-  
-  //collapse other items
   jQuery('.js-accordion__item').not(this).addClass('state-collapsed');
-  
-  
 });
 
 
 
 jQuery(document).ready(function() {
-    jQuery('.-accordion').asAccordion({
-      namespace: '-accordion'
-    });
-  });
-
-  jQuery(document).ready(function() {
-    jQuery('.-accordion').asAccordion({
-      namespace: '-accordion',
-      // accordion theme. WIP
-      skin: null,
-  
-      // breakpoint for mobile devices. WIP
-      mobileBreakpoint: 768,
-  
-      // initial index panel
-      initialIndex: 0,
-  
-      // CSS3 easing effects.
-      easing: 'ease-in-out',
-  
-      // animation speed.
-      speed: 500,
-  
-      // vertical or horizontal
-      direction: 'vertical',
-  
-      // jQuery mouse events. click, mousehover, etc.
-      event: 'click',
-  
-      // multiple instance
-      multiple: false
-    });
-  });
+    jQuery('.custom-navbar-toggle').click(function() {
+      jQuery(this).toggleClass('menu-open')
+    })
+});
