0b9ae999ecb634f8c4cc00ae985c90950acdc1d8
supriyofrontend <supriyo.codearts@gmail.com>
2022-12-06 17:35:23 +0530

    css changes added

--- js/custom.js
+++ js/custom.js
@@ -85,6 +85,7 @@ jQuery('#team-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     responsive:{
         0:{
             items:1
